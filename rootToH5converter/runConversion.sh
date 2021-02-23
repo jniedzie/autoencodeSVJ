ade0b8ec612d552ca66f8a6bf7e12a1174a5c7b1
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2021-02-23 11:13:45 +0100

    Updating h5 converter

---
+++ rootToH5converter/runConversion.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+cd /afs/cern.ch/work/j/jniedzie/private/svjets/autoencodeSVJ/rootToH5converter
+
+echo "Activating conda"
+
+#. /etc/profile.d/conda.sh
+source /usr/lib/python3.6/site-packages/conda/shell/etc/profile.d/conda.sh
+
+#eval "$(conda shell.bash hook)"
+#conda init bash
+conda activate ml
+
+echo "Python version:"
+python --version
+
+echo "Setting up ROOT"
+
+. /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.22.02/x86_64-centos7-gcc48-opt/bin/thisroot.sh
+
+echo "Figuring out mass and r inv"
+
+# find out sample name from the index
+i=$(($1-1))
+ir=$(($i / 6))
+im=$(($i % 6))
+
+masses=(1500 2000 2500 3000 3500 4000)
+rinvs=(15 30 45 60 75)
+
+mass=${masses[$im]}
+rinv=${rinvs[$ir]}
+
+echo "Running for mass: ${mass}, r_inv: ${rinv}"
+
+selections_path=../preselection/results/all_cuts/SVJ_m${mass}_r${rinv}_selection.txt
+output_path=results/all_cuts/SVJ_m${mass}_r${rinv}.h5
+
+python rootToH5.py -i $selections_path -o $output_path -e 3
--- rootToH5converter/setup.sh
+++ rootToH5converter/setup.sh
@@ -1,5 +1,8 @@
 source /cvmfs/sft.cern.ch/lcg/views/LCG_93/x86_64-centos7-gcc7-opt/setup.sh
 
+conda activate ml
+source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.22.02/x86_64-centos7-gcc48-opt/bin/thisroot.sh
+
 python -c "import energyflow"
 if [ $? -eq 1 ]
 then 
