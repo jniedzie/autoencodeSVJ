3a7d2b5b994aa6a7434c4f122b26c3b8da202adc
jniedzie <jeremi.niedziela@cern.ch>
2021-03-11 16:49:03 +0100

    Extending drawROCcurves for the BDT case

--- training/drawROCcurves.py
+++ training/drawROCcurves.py
@@ -1,5 +1,6 @@
 import module.SummaryProcessor as summaryProcessor
 from module.AutoEncoderEvaluator import AutoEncoderEvaluator
+from module.BdtEvaluator import BdtEvaluator
 import importlib, argparse
 
 # ------------------------------------------------------------------------------------------------
@@ -14,21 +15,32 @@ args = parser.parse_args()
 config_path = args.config_path.strip(".py").replace("/", ".")
 config = importlib.import_module(config_path)
 
-input_summary_path = summaryProcessor.get_latest_summary_file_path(
-    summaries_path=config.summary_path,
-    file_name_base=config.file_name,
-    version=config.best_model)
+if config.model_type == "AutoEncoder":
 
-
-# masses = [1500, 2000, 2500, 3000, 3500, 4000]
-masses = [2000]
-rinvs = [0.15, 0.30, 0.45, 0.60, 0.75]
-
-signals = {"{}, {}".format(mass, rinv) : "{}{}GeV_{:1.2f}/base_3/*.h5".format(config.signals_base_path, mass, rinv)
-           for mass in masses
-           for rinv in rinvs}
-
-print("\n\nDraing ROC curves for summary: ", input_summary_path)
-
-evaluator = AutoEncoderEvaluator(input_summary_path, signals=signals)
-evaluator.draw_ROCs(xscale='log', metrics=["mae"])
+    input_summary_path = summaryProcessor.get_latest_summary_file_path(
+        summaries_path=config.summary_path,
+        file_name_base=config.file_name,
+        version=config.best_model)
+    
+    
+    # masses = [1500, 2000, 2500, 3000, 3500, 4000]
+    masses = [2000]
+    rinvs = [0.15, 0.30, 0.45, 0.60, 0.75]
+    
+    signals = {"{}, {}".format(mass, rinv) : "{}{}GeV_{:1.2f}/base_3/*.h5".format(config.signals_base_path, mass, rinv)
+               for mass in masses
+               for rinv in rinvs}
+    
+    print("\n\nDraing ROC curves for summary: ", input_summary_path)
+    
+    evaluator = AutoEncoderEvaluator(input_summary_path, signals=signals)
+    evaluator.draw_ROCs(xscale='log', metrics=["mae"])
+    
+elif config.model_type == "BDT":
+    evaluator = BdtEvaluator()
+    evaluator.draw_ROCs(summary_path=config.summary_path,
+                        signals_base_path=config.signals_base_path,
+                        metrics=["mae"],
+                        xscale='log')
+else:
+    print("Unknown model type: ", config.model_type)
