9762548da83a671a5275b0d604ee8d6764fd9bf4
jniedzie <jeremi.niedziela@cern.ch>
2021-03-11 16:49:46 +0100

    Attempt to draw ROC curves for BDT case

--- training/module/BdtEvaluator.py
+++ training/module/BdtEvaluator.py
@@ -2,7 +2,9 @@ import module.utils as utils
 import module.SummaryProcessor as summaryProcessor
 from module.DataProcessor import DataProcessor
 from module.DataLoader import DataLoader
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, plot_roc_curve
+
+import matplotlib.pyplot as plt
 
 import numpy as np
 import pandas as pd
@@ -15,16 +17,87 @@ class BdtEvaluator:
     def __init__(self):
         pass
     
-    def draw_ROCs(self, metrics=None, figsize=8, figloc=(0.3, 0.2), *args, **kwargs):
-        
-        if metrics is None:
-            metrics=['mae', 'mse']
-        
-        qcd = self.errs_dict['qcd']
-        others = [self.errs_dict[n] for n in self.all_names if n != 'qcd']
-        
-        utils.roc_auc_plot(qcd, others, metrics=metrics, figsize=figsize, figloc=figloc, *args, **kwargs)
-        return
+    # def draw_ROCs(self, summary_path, signals_base_path, metrics=None, figsize=8, figloc=(0.3, 0.2), *args, **kwargs):
+    #
+    #     if metrics is None:
+    #         metrics=['mae', 'mse']
+    #
+    #     summaries = summaryProcessor.summary(summary_path=summary_path)
+    #
+    #     data = {}
+    #     qcd_errors = None
+    #     signal_errors = []
+    #
+    #     for index, summary in summaries.df.iterrows():
+    #         if index == 2:
+    #             break
+    #
+    #         data_processor = DataProcessor(validation_fraction=summary.val_split,
+    #                                        test_fraction=summary.test_split,
+    #                                        seed=summary.seed
+    #                                        )
+    #
+    #         qcd_test, qcd_test_labels = BdtEvaluator.get_data(data_path=summary.qcd_path,
+    #                                             is_background=True,
+    #                                             data_processor=data_processor,
+    #                                             include_hlf=summary.hlf,
+    #                                             include_eflow=summary.eflow,
+    #                                             hlf_to_drop=summary.hlf_to_drop
+    #                                             )
+    #
+    #         path = summary.training_output_path
+    #         filename = path.split("/")[-1]
+    #         signal_components = filename.split("_")
+    #         mass_index = [i for i, s in enumerate(signal_components) if 'GeV' in s][0]
+    #
+    #         mass = int(signal_components[mass_index].strip("GeV"))
+    #         rinv = float(signal_components[mass_index + 1])
+    #
+    #         signal_name = "{}GeV_{:3.2f}".format(mass, rinv)
+    #         signal_path = signals_base_path + "/" + signal_name + "/base_3/*.h5"
+    #
+    #         svj_test, svj_test_labels = BdtEvaluator.get_data(data_path=signal_path,
+    #                                                        is_background=False,
+    #                                                        data_processor=data_processor,
+    #                                                        include_hlf=summary.hlf,
+    #                                                        include_eflow=summary.eflow,
+    #                                                        hlf_to_drop=summary.hlf_to_drop
+    #                                                        )
+    #
+    #         qcd_test = qcd_test.append(svj_test)
+    #         qcd_test_labels = qcd_test_labels.append(svj_test_labels)
+    #
+    #
+    #
+    #         # if "qcd" not in data.keys():
+    #         #     data = {"qcd": qcd_test}
+    #         #
+    #         # data[signal_name] = svj_test
+    #
+    #         model_path = path + ".weigths"
+    #         try:
+    #             print("Reading file: ", model_path)
+    #             model = open(model_path, 'rb')
+    #         except IOError:
+    #             print("Couldn't open file ", model_path, ". Skipping...")
+    #             continue
+    #         bdt = pickle.load(model)
+    #
+    #         # errors, _ = utils.get_recon_errors({"qcd": data["qcd"], "svj": svj_test}, bdt)
+    #
+    #         roc_curve(y_true, y_score, *, pos_label=None, sample_weight=None, drop_intermediate=True)[source]
+    #
+    #         plot_roc_curve(bdt, qcd_test, qcd_test_labels)
+    #
+    #     plt.show()
+    #         # if qcd_errors is None:
+    #         #     qcd_errors = errors["qcd"]
+    #
+    #         # signal_errors.append([value for key, value in errors if key != "qcd"])
+    #
+    #     # utils.roc_auc_plot(qcd_errors, signal_errors, metrics=metrics, figsize=figsize, figloc=figloc, *args, **kwargs)
+    #
+    #     return
 
     @staticmethod
     def get_data(data_path, is_background, data_processor,
@@ -43,6 +116,8 @@ class BdtEvaluator:
         data_Y_test = pd.DataFrame(fun((len(data_X_test.df), 1)), index=data_X_test.index, columns=['tag'])
         
         return data_X_test, data_Y_test
+
+    
 
     @staticmethod
     def save_AUCs(signals_base_path, AUCs_path, summary_path):
