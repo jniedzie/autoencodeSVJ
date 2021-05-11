49e931b76dbfbab79b6d7a249c9b444bda0c85fe
Jeremi Niedziela (ETHZ) [jniedzie] <jniedzie@t3ui02.psi.ch>
2021-05-11 08:33:15 +0200

    Updating preselection script to work on T3

--- preselection/runSelections.sh
+++ preselection/runSelections.sh
@@ -1,5 +1,40 @@
 #!/bin/bash
 
+declare -a workdirs=(
+  "/afs/cern.ch/work/j/jniedzie/private/svjets/autoencodeSVJ/preselection"
+  "/t3home/jniedzie/autoencodeSVJ/preselection"
+)
+
+found_workdir=false
+ 
+
+for workdir in ${workdirs[@]}; do
+  echo "Trying to move to ${workdir}"
+  if [[ -d "$workdir" ]]
+  then
+    cd $workdir
+    echo "moving to $workdir"
+    found_workdir=true
+    break
+  fi
+done
+
+if [ $found_workdir != true ]; then
+  echo "Couldn't find working directory..."
+  exit
+fi
+
+
+string='My long string'
+if [[ $HOSTNAME == *"lxplus"* ]]; then
+  echo "Setting environment for lxplus"
+  . setenv.sh
+else
+  echo "Setting environemtn for eth T3"
+fi
+
+
+
 output_path=results/no_lepton_veto_fat_jets/
 #output_path=results/no_lepton_veto_ak4_jets/
 #output_path=results/all_cuts_fat_jets/
@@ -8,8 +43,7 @@ output_path=results/no_lepton_veto_fat_jets/
 nEvents=$1
 i=$(($2-1))
 
-cd /afs/cern.ch/work/j/jniedzie/private/svjets/autoencodeSVJ/preselection
-. setenv.sh
+
 
 mkdir -p $output_path
 
@@ -24,4 +58,9 @@ rinv=${rinvs[$ir]}
 
 echo "Running for mass: ${mass}, r_inv: ${rinv}"
 
-./SVJselection inputFileLists/input_file_list_m${mass}_r${rinv}.txt SVJ_m${mass}_r${rinv} ${output_path} 0 $nEvents
+#input_path=inputFileLists/input_file_list_m${mass}_r${rinv}.txt
+input_path=inputFileLists/input_file_list_m3500_mDark20_r30_alphaPeak.txt
+#sample_name=SVJ_m${mass}_r${rinv}
+sample_name=SVJ_m3500_mDark20_r30_alphaPeak
+
+./SVJselection $input_path $sample_name $output_path 0 $nEvents
